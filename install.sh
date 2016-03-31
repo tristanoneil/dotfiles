17514a85137a8098ac82eec89f50ccf242fd5b03
Tristan O'Neil <tristanoneil@gmail.com>
2016-03-31 20:43:30 +0000

    Add some docs

--- install.sh
+++ install.sh
@@ -11,8 +11,13 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
+# download base-16 colors
 wget https://raw.githubusercontent.com/chriskempson/base16-shell/master/base16-ocean.dark.sh -O ~/.base16-ocean.dark.sh
+
+# download sensible bash
 wget https://raw.githubusercontent.com/mrzool/bash-sensible/master/sensible.bash -O ~/.sensible.bash
+
+# download vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks

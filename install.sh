9c6407c88b5df85417609d583e656ef1282d8578
Tristan O'Neil <tristanoneil@gmail.com>
2016-04-13 09:36:03 -0400

    Switch to ZSH

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 dir=~/dotfiles
 
  # list of files/folders to symlink in homedir
-files="vim gemrc gitconfig gitignore_global vimrc bash_profile inputrc tmux.conf"
+files="vim gemrc gitconfig gitignore_global vimrc tmux.conf"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -14,11 +14,17 @@ echo "...done"
 # download base-16 colors
 wget https://raw.githubusercontent.com/chriskempson/base16-shell/master/base16-ocean.dark.sh -O ~/.base16-ocean.dark.sh
 
-# download sensible bash
-wget https://raw.githubusercontent.com/mrzool/bash-sensible/master/sensible.bash -O ~/.sensible.bash
-
 # download vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# create custom zsh themes dir
+mkdir $ZSH_CUSTOM/themes
+
+# download zsh lambda theme
+wget https://raw.githubusercontent.com/halfo/lambda-mod-zsh-theme/master/lambda-mod.zsh-theme -O $ZSH_CUSTOM/themes/lambda-mod.zsh-theme
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do

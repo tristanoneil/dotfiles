7a378fa1e57c5eeb3990c63274329a6b088ae042
Tristan O'Neil <tristanoneil@gmail.com>
2016-04-14 15:19:11 -0400

    Add default zshrc

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 dir=~/dotfiles
 
  # list of files/folders to symlink in homedir
-files="vim gemrc gitconfig gitignore_global vimrc tmux.conf"
+files="vim gemrc gitconfig gitignore_global vimrc tmux.conf zshrc"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -17,11 +17,13 @@ wget https://raw.githubusercontent.com/chriskempson/base16-shell/master/base16-o
 # download vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# install vundle plugins
+vim +PluginInstall +qall
+
 # install oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sudo chsh -s $(which zsh) vagrant
-. ~/.zshrc
+source zshrc
 
 # create custom zsh themes dir
 mkdir $ZSH_CUSTOM/themes

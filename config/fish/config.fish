2f7fb8e80f369054d9ebc1ded700433bdac2a705
Tristan O'Neil <tristanoneil@gmail.com>
2016-04-27 01:38:32 +0000

    Switch back to the Fish

---
+++ config/fish/config.fish
@@ -0,0 +1,4 @@
+alias s="git status"
+
+bass source ~/.nvm/nvm.sh --no-use
+bass source ~/.rvm/scripts/rvm
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 dir=~/dotfiles
 
  # list of files/folders to symlink in homedir
-files="vim gemrc gitconfig gitignore_global vimrc tmux.conf zshrc"
+files="vim gemrc gitconfig gitignore_global vimrc tmux.conf config"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -17,16 +17,8 @@ wget https://raw.githubusercontent.com/chriskempson/base16-shell/master/base16-o
 # download vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-sudo chsh -s $(which zsh) vagrant
-source zshrc
-
-# create custom zsh themes dir
-mkdir $ZSH_CUSTOM/themes
-
-# download zsh lambda theme
-wget https://raw.githubusercontent.com/halfo/lambda-mod-zsh-theme/master/lambda-mod.zsh-theme -O $ZSH_CUSTOM/themes/lambda-mod.zsh-theme
+# install fisherman
+curl -Lo config/fish/functions/fisher.fish --create-dirs git.io/fisherman
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
@@ -36,3 +28,6 @@ done
 
 # install vundle plugins
 vim +PluginInstall +qall
+
+# install fish plugins
+fisher up

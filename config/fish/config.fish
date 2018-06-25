065642edf939148159aaf78a2ebee22a10172a0d
Tristan O'Neil <tristanoneil@gmail.com>
2018-06-25 17:17:56 -0400

    Don't need any of this anymore

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,12 +6,4 @@ alias pull="git pull origin HEAD"
 alias push="git push origin head"
 alias s="git status"
 
-set -x LC_ALL en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
-
 set -gx EDITOR vim
-set -gx TERM xterm-256color-italic
-set -gx WORKON_PATH ~/.workon
-
-# for rbenv
-status --is-interactive; and source (rbenv init -|psub)

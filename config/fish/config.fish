c6e7ef2e1de336403bba3139bbadd4a3f4ca6f07
Tristan O'Neil <tristanoneil@gmail.com>
2019-02-17 12:08:36 -0500

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,16 @@
 alias am="git commit --amend"
 alias b="git branch"
-alias c="git checkout"
+alias co="git checkout"
+alias cop="rubocop (git diff HEAD --name-only --diff-filter=MA & git diff master..HEAD --name-only --diff-filter=MA | sort | uniq)"
 alias dev="cd ~/src"
 alias pull="git pull origin HEAD"
 alias push="git push origin head"
 alias s="git status"
+alias go="tmuxinator start maji"
 
 set -gx EDITOR vim
 set -gx TERM xterm-256color-italic
+set fish_greeting
 
 if type -q rbenv
   source (rbenv init - | psub)

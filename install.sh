1b00f191a8ef9cf8eedcfba154739d8f560db191
Tristan O'Neil <to@tristanoneil.com>
2019-12-03 20:24:40 -0500

    Some updates

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mv ~/.config ~/.config-bak
 dir=~/dotfiles
 
  # list of files/folders to symlink in homedir
-files="vim gitconfig gitignore_global vimrc config gemrc rspec"
+files="vim gitconfig gitignore_global vimrc config gemrc rspec tmux"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
